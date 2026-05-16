68175f54163ff5846f811e72b4a17d70ed74d569
atp <atpatp0816@gmail.com>
2026-05-16 09:31:23 +0900

    상하구분 기능 추가함

--- app2.py
+++ app2.py
@@ -20,16 +20,9 @@ line_df = df[df["호선"] == selected_line]
 station_list = sorted(line_df["출발역"].dropna().unique())
 selected_station = st.selectbox("역을 선택하세요", station_list)
 
+
 # 선택한 노선 + 역에 해당하는 데이터 필터링
 station_df = line_df[line_df["출발역"] == selected_station]
-
-st.subheader("선택한 조건")
-st.write(f"선택한 노선: {selected_line}")
-st.write(f"선택한 역: {selected_station}")
-
-st.subheader("선택한 역 데이터")
-st.dataframe(station_df)
-
 
 # 요일 선택
 day_list = sorted(station_df["요일구분"].dropna().unique())
@@ -38,10 +31,23 @@ selected_day = st.selectbox("요일을 선택하세요", day_list)
 # 선택한 노선 + 역 + 요일에 해당하는 데이터 필터링
 day_df = station_df[station_df["요일구분"] == selected_day]
 
+
+#상하구분 선택
+
+direction_list = sorted(station_df["상하구분"].dropna().unique())
+selected_direction = st.selectbox("상하구분을 선택하세요", direction_list)
+
+# 선택한 노선 + 역 + 상하구분에 해당하는 데이터 필터링
+direction_df = station_df[station_df["상하구분"] == selected_direction]
+
+
+
+
 st.subheader("선택한 조건")
 st.write(f"선택한 노선: {selected_line}")
 st.write(f"선택한 역: {selected_station}")
 st.write(f"선택한 요일: {selected_day}")
+st.write(f"선택한 상하구분: {selected_direction}")
+st.subheader("선택한 조건의 원본 데이터")
+st.dataframe(direction_df)
 
-st.subheader("선택한 요일 데이터")
-st.dataframe(day_df)
